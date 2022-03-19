1f12625b5add79d63c6241d1ec3a3fb6174a3c3f
Stéphane Sordet <stsordet@gmail.com>
2022-03-19 11:16:09 +0100

    load movies function added

---
+++ code/src/index.js
@@ -0,0 +1,14 @@
+import * as d3 from 'd3';
+import * as Movies from './loadMovies';
+ 
+const TitlesArray = []
+const IntroductionArray = []
+const StudiosArray = []
+const ReleaseArray = []
+const DomSalesArray = []
+const IntSalesArray = []
+const WorldSalesArray = []
+const GenreArray = []
+const RuntimeArray = []
+
+console.log(Movies.getStudio())
