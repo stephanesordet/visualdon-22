223db58e998da59dae0281deb8ba9c6c72b008d0
Stéphane Sordet <stsordet@gmail.com>
2022-05-06 14:10:17 +0200

    Exercice 5: OK

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -3,17 +3,15 @@ import dataIncome from '../data/income_per_person_gdppercapita_ppp_inflation_adj
 import dataLifeExpectancy from '../data/life_expectancy_years.csv'
 import dataPopulation from '../data/population_total.csv'
 
-/*
-for (var i = 0; i < dataIncome.length; i++) {
-        console.log(dataIncome[i].country);
-}
-for (var i = 0; i < dataLifeExpectancy.length; i++) {
-    console.log(dataLifeExpectancy[i].country);
-}
-*/
-for (var i = 0; i < dataPopulation.length; i++) {
-    console.log(cleanData(dataPopulation[i]["2021"]));
-}
+const margin = {
+    top: 50,
+    right: 10,
+    bottom: 0,
+    left: 100
+},
+width = window.innerWidth * 0.7 - margin.left - margin.right,
+height = window.innerHeight * 0.9 - margin.top - margin.bottom;
+
 
 //Scale
 const maxGDP = d3.max(dataIncome, function(d) { return cleanData(d[2021]); });
@@ -23,70 +21,147 @@ const minExpectancy = d3.min(dataLifeExpectancy, function(d){return cleanData(d[
 const maxPop = d3.max(dataPopulation, function(d) { return cleanData(d[2021]); })
 const minPop = d3.min(dataPopulation, function(d){return cleanData(d[2021]); })
 
-console.log(minExpectancy)
-
-const margin = {
-        top: 50,
-        right: 10,
-        bottom: 0,
-        left: 100
-    },
-    width = window.innerWidth * 0.7 - margin.left - margin.right,
-    height = window.innerHeight * 0.9 - margin.top - margin.bottom;
-
 const svgGraph = d3.select('body').append('svg').attr('class', 'graph');
 
 svgGraph.attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 const x = d3.scaleLinear()
-    .domain([minGDP, maxGDP])
-    .range([10, width]);
+.domain([minGDP, maxGDP])
+.range([10, width]);
 
 svgGraph.append('g')
-    .attr("transform", "translate(5," + height + ")")
-    .call(d3.axisTop(x).ticks(35).tickSize(10)).selectAll("text")
-    .style("text-anchor", "end")
-    .attr("dx", "-0.6em")
-    .attr("dy", "2.4em")
-    .attr("transform", "rotate(-65)");
+.attr("transform", "translate(5," + height + ")")
+.call(d3.axisTop(x).ticks(35).tickSize(10)).selectAll("text")
+.style("text-anchor", "end")
+.attr("dx", "-0.6em")
+.attr("dy", "2.4em")
+.attr("transform", "rotate(-65)");
 
 //Pow pour avoir une échelle exponentielle qui se concentre vers 0
 const y = d3.scalePow()
-    .domain([0, maxExpectancy])
-    .range([height, 0])
-    .exponent(3);
+.domain([0, maxExpectancy])
+.range([height, 0])
+.exponent(3);
 
 svgGraph.append('g')
-    .call(d3.axisRight(y).ticks(10)); 
-    
+.call(d3.axisRight(y).ticks(10)); 
+
 //Sqrt pour équilibrer les tailles des cercles 
 const r = d3.scaleSqrt()
-    .domain([minPop, maxPop])
-    .range([0, 30]);
+.domain([minPop, maxPop])
+.range([0, 30]);
 
 //Circles
 for (var i = 0; i < dataPopulation.length; i++) {
-    svgGraph.append("circle")
-    .attr("cx", x(cleanData(dataIncome[i]["2021"]))).attr("cy", y(dataLifeExpectancy[i]["2021"])).attr("r", r(cleanData(dataPopulation[i]["2021"]))).style("fill", "blue");
+svgGraph.append("circle")
+.attr("cx", x(cleanData(dataIncome[i]["2021"]))).attr("cy", y(dataLifeExpectancy[i]["2021"])).attr("r", r(cleanData(dataPopulation[i]["2021"]))) .style("fill", "blue");
 }
 
 //Clean Data
 function cleanData(data) {
-    if (isNaN(data)) {
-        if (data.includes("k")) {
-            const n = data.split("k")[0];
-            return Number.parseFloat(n) * 1000;
-        } else if (data.includes("M")) {
-            const n = data.split("M")[0];
-            return Number.parseFloat(n) * 1000000;
+if (isNaN(data)) {
+    if (data.includes("k")) {
+        const n = data.split("k")[0];
+        return Number.parseFloat(n) * 1000;
+    } else if (data.includes("M")) {
+        const n = data.split("M")[0];
+        return Number.parseFloat(n) * 1000000;
 
-        } else if (data.includes("B")) {
-            const n = data.split("B")[0];
-            return Number.parseFloat(n) * 1000000000;
-        }
+    } else if (data.includes("B")) {
+        const n = data.split("B")[0];
+        return Number.parseFloat(n) * 1000000000;
     }
-    return data;
 }
+return data;
+}
+
+function getData(currentYear) {
+	const currentLife = dataLifeExpectancy.map((year) => {
+		return { country: year["country"], life: year[currentYear] };
+	});
+	const currentPop = dataPopulation.map((year) => {
+		return { country: year["country"], pop: year[currentYear] };
+	});
+	const currentIncome = dataIncome.map((year) => {
+		return { country: year["country"], income: year[currentYear] };
+	});
+	const nextLife = dataLifeExpectancy.map((year) => {
+		return { country: year["country"], life: year[currentYear + 1] };
+	});
+	const nextPop = dataPopulation.map((year) => {
+		return { country: year["country"], pop: year[currentYear + 1] };
+	});
+	const nextIncome = dataIncome.map((year) => {
+		return { country: year["country"], income: year[currentYear + 1] };
+	});
+
+    let data = [];
+	for (let i = 0; i < dataIncome.length; i++) {
+		data.push({
+			country: currentIncome[i].country,
+			currentPop: cleanData(currentPop[i].pop),
+			currentLife: cleanData(currentLife[i].life),
+			currentIncome: cleanData(currentIncome[i].income),
+			nextPop: cleanData(nextPop[i].pop),
+			nextLife: cleanData(nextLife[i].life),
+			nextIncome: cleanData(nextIncome[i].income),
+		});
+	}
+	return data;
+}
+
+let t = d3.transition().duration(1000).ease(d3.easeLinear);
+const firstYear = 1980;
+const lastYear = 2021;
+const svgGraph2 = d3.select("body").append("svg");
+let currentYear = firstYear;
+
+svgGraph2.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svgGraph2.append('g')
+.attr("transform", "translate(5," + height + ")")
+.call(d3.axisTop(x).ticks(35).tickSize(10)).selectAll("text")
+.style("text-anchor", "end")
+.attr("dx", "-0.6em")
+.attr("dy", "2.4em")
+.attr("transform", "rotate(-65)"); 
+
+svgGraph2.append('g')
+.call(d3.axisRight(y).ticks(10));
+
+(function draw() {
+	const data = getData(currentYear);
+
+	svgGraph2
+		.selectAll("circle")
+		.data(data)
+		.join(
+			(enter) =>
+				enter
+					.append("circle")
+					.attr("cx", (d) => x(d.currentIncome))
+					.attr("cy", (d) => y(d.currentLife))
+					.attr("r", (d) => r(d.currentPop))
+					.attr("class", (d) => `countryCircle ${d.country}`),
+			(update) =>
+				update
+					.transition()
+					.duration(1000)
+					.attr("cx", (d) => x(d.nextIncome))
+					.attr("cy", (d) => y(d.nextLife))
+					.attr("r", (d) => r(d.nextPop)),
+			(exit) => exit.attr("r", (d) => r(d.nextPop)).remove()
+		)
+		.style("fill", "rgba(100, 0, 0, 0.4)")
+		.attr("transform", "translate(100, 10)");
+	if (currentYear < lastYear) {
+		currentYear++;
+		setTimeout(draw, 1000);
+	}
+})();
--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -1,4 +1,60 @@
-import jsdom from "jsdom";
+import {JSDOM} from "jsdom";
 import fetch from "isomorphic-fetch"
 import puppeteer from "puppeteer"
 
+(async () => {
+    //Crée un nouveau browser
+    const browser = await puppeteer.launch();
+    //Créer une nouvelle instance de la page
+    const page = await browser.newPage();
+
+    //Go to Wikipedia
+    await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
+
+    //Prend un screenshot
+    const screen = await page.screenshot({ path: 'cantons.png', fullPage: true });
+    console.log(screen);
+
+    //Ferme le browser
+    await browser.close();
+})();
+
+//Cantons suisses 
+(async () => {
+    const response = await fetch('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
+    const text = await response.text();
+    const dom = new JSDOM(text);
+    const cantons = dom.window.document.querySelectorAll("table.wikitable tr");
+    for (const canton of cantons) {
+        try {
+            const name = canton.querySelector("td a").textContent;
+            const population = canton.querySelector("td bdi").textContent;
+            let populationNumber = population.substring(1, population.length - 1);
+            if (populationNumber.startsWith("00")) {
+                populationNumber = populationNumber.substring(2, populationNumber.length);
+            } else if (populationNumber.startsWith("0")) {
+                populationNumber = populationNumber.substring(1, populationNumber.length);
+            }
+            console.log(`${name} : ${populationNumber}`);
+        } catch (error) { }
+    }
+})();
+
+
+//E-commerce
+(async () => {
+    const response = await fetch('https://www.webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+    const text = await response.text();
+    const dom = new JSDOM(text);
+    const products = dom.window.document.querySelectorAll("div.thumbnail ");
+    for (const product of products) {
+        try {
+            const line = {
+                "product" : product.querySelector("div.caption h4 a").textContent,
+                "price" : product.querySelector("div.caption h4").textContent,
+                "ratings" : product.querySelectorAll("div.ratings p span").length
+            }
+            console.log(line);
+        } catch (error) {}   
+    }
+})()
